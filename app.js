9c5da8767f4c1cb02d02a6500f13524cc9379505
unknown <xlanimation@gmail.com>
2021-05-12 14:21:09 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+
+
+let friends = ["Chris", "Mark", "Cameron", "Erskine", "James"];
+let text = "";
+let i;
+let j;
+
+// for (let i = 0; i < friends.length; i++) {
+
+//     if (friends[i] === "Chris") {
+//         alert("This worked");
+//         continue;
+//     }
+
+// }
+
+
+loop1:
+for (i = 0; i < friends.length; i++) {
+    
+    loop2:
+    for (j = 99; j >= 0; j--) {
+        let minusOne = j - 1;
+        let name = friends[i];
+        if (j === 2) {
+            console.log(j + ' lines of code in the file, ' + i + ' lines of code; ' + name + ' strikes one out, clears it all out, ' + minusOne + ' line of code in the file.');
+        }
+        else if (j === 1) {
+            let minusOne = 'no more';
+            console.log(j + ' line of code in the file, ' + i + ' line of code; ' + name + ' strikes one out, clears it all out, ' + minusOne + ' lines of code in the file.');
+        }
+        else if (j === 0) {
+            let i = 'No more';
+            console.log(j + ' lines of code in the file, ' + i.toLowerCase() + ' lines of code; ' + name + ' types some more, writes some code, ' + minusOne + ' lines of code in the file.');
+        }
+        else {
+            console.log(j + ' lines of code in the file, ' + i + ' lines of code; ' + name + ' strikes one out, clears it all out, ' + minusOne + ' lines of code in the file.');
+        }
+    }
+
+}
